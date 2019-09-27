8957a94ae64e45511275f65efb6190b1d1218bfa
Samet Özdemir <48583302+SametZdemir@users.noreply.github.com>
2019-09-27 17:22:09 +0300

    Add files via upload

---
+++ kamera_slider.ino
@@ -0,0 +1,36 @@
+int steppin=4;
+int dirpin=3;
+int value;
+int buton =5;
+
+void setup() {
+
+  pinMode(5,INPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  
+
+}
+
+void loop() {
+  int x=digitalRead(buton);
+  int deger=analogRead(A0);
+  value=map(deger,0,1023,900,5000);
+   if(x == 1){
+    digitalWrite(3,LOW);
+  digitalWrite(steppin,HIGH);
+  delayMicroseconds(value);
+  digitalWrite(steppin,LOW);
+  delayMicroseconds(value);
+  }
+  else {
+    digitalWrite(3,HIGH);
+  digitalWrite(steppin,HIGH);
+  delayMicroseconds(value);
+  digitalWrite(steppin,LOW);
+  delayMicroseconds(value);
+    
+  }
+  
+
+}
